bc18711e6ffad6ae7bd5b35561e1f070a995bbc5
pythonkila <52569772+pythonkila@users.noreply.github.com>
2020-05-10 12:42:55 -0500

    Update shoutout.py

--- fun/shoutout.py
+++ fun/shoutout.py
@@ -1,13 +1,19 @@
-# 09 May 2020
+# 10 May 2020
 # The code takes in an input ( a greeting ) and shout out greeting n number of time.
 
-hail = input('Enter your shoutout!...').upper()
-reps = int(input('how many time will you want this repeated...'))
-for x in hail.lower():
-    if x in 'aeiou':
+hail = input('Enter your shoutout!...').upper()                                  
+while hail.isalpha() == False:
+    hail = input('Enter a valid shoutout!...').upper()    
+reps = input('How many Gbosa !...')
+while reps.isdigit()  == False:
+    reps = input('How many time will you want this repeated...')
+for x in hail:
+    if x in 'AEIOU':
         print('Can i get an',x,'!')
-    elif x not in 'aeiou':
+    elif x not in 'AEIOU':
         print('Can i get a',x,'!')
+reps = int(reps)
+print('Can I get',reps,'Gbosa for my fellow!')
 while reps > 0:
-    print(hail.upper()+'!')
-    reps -= 1
+    print(reps,'Gbosa for',hail.upper()+'!')
+    reps -= 1    
